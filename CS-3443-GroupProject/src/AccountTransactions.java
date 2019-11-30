f1d56fb477dfed922531f7a315114b384fab8e3c
njmau <njmau@Nathan-Desktop>
2019-11-29 19:24:37 -0600

    Account pages and controllers

---
+++ CS-3443-GroupProject/src/AccountTransactions.java
@@ -0,0 +1,44 @@
+
+
+public class AccountTransactions {
+	private String date;
+	private String transactionDescription;
+	private float amount;
+	private static float balance;
+	
+	public AccountTransactions(String date, String transactionDescription, float amount, float balance) {
+		this.date = date;
+		this.transactionDescription = transactionDescription;
+		this.amount = amount;
+		AccountTransactions.balance = balance;
+	}
+	public String getDate()
+	{
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String transactionDescription() {
+		return transactionDescription;
+	}
+	public void setTransactionDescriptions(String transactionDescription) {
+		this.transactionDescription = transactionDescription;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	public float getBalance() {
+		return balance;
+	}
+	public void setBalance(float balance) {
+		AccountTransactions.balance = balance;
+	}
+	
+	public String toString() {
+		return (this.date + "\t" + this.transactionDescription + "\t" + String.valueOf(this.amount) + "\t\t" + String.valueOf(this.balance));
+	}
+}
